6790c9347bafc63e907c0c02048aaf32ac777d44
alderney ramirez <alderneyramirez@gmail.com>
2025-09-10 10:20:49 -0500

    se migra el chekout con estilos en taiwild y logica incorporoda

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Carrito from './components/Carrito'
+
+import Checkout from './pages/checkout'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Carrito />
+    <Checkout />
   </StrictMode>,
 )
 
